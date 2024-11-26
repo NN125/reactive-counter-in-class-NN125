9385b28beea6f83ab4e0f0115752fed288619da7
NN125 <nicolas.nolden@student.swic.edu>
2024-11-25 20:36:42 -0600

    Completely redo main.js

--- src/main.js
+++ src/main.js
@@ -1,24 +1,23 @@
-let count = 0;
+import Button from "./components/button";
+import Count from "./components/count";
+import state from "./state";
+
+const app = document.querySelector("#app");
 
 function render() {
-  document.getElementById("count-display").textContent = count;
+  app.innerHTML = `  ${Count(state.getState().count)}
+    <div class="flex justify-center">
+      ${Button("+1", "increment")}
+      ${Button("-1", "decrement")}
+    </div>`;
 }
 
-function setCount(newCount) {
-  count = newCount;
-  render();
-}
-
-document.getElementById("increment").addEventListener("click", () => {
-  setCount(count + 1);
-});
-
-document.getElementById("decrement").addEventListener("click", () => {
-  setCount(count - 1);
-});
-
-document.getElementById("reset").addEventListener("click", () => {
-  setCount(0);
-});
-
+state.subscribe(render);
 render();
+
+document.addEventListener("click", (event) => {
+  if (event.target.id === "increment")
+    state.setState({ count: state.getState().count + 1 });
+  else if (event.target.id === "decrement")
+    state.setState({ count: state.getState().count - 1 });
+});
